ccb895467fdd733508ed324f9a18a05fca761f38
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-05-25 19:12:09 -0600

    Added little movement up and down

--- Assets/ClawAndFeather/Scripts/PlayerController.cs
+++ Assets/ClawAndFeather/Scripts/PlayerController.cs
@@ -7,8 +7,12 @@ using UnityEngine.InputSystem.Interactions;
 public class PlayerController : MonoBehaviour
 {
     #region Movement
+    private float _verticalDir = 0;
+    [SerializeField] private float _angleMult = 10;
     private float _direction = 1; // 1 is right, -1 is left
     public float Speed { get; set; } = 5f;
+    public Vector2 Velocity { get; set; }
+    
     #endregion
 
     void Start()
@@ -17,7 +21,9 @@ public class PlayerController : MonoBehaviour
     }
     void Update()
     {
-        transform.Translate(_direction * Vector2.right * Speed * Time.deltaTime);
+        _verticalDir = (Time.deltaTime * _angleMult + _verticalDir) % 360;
+        Velocity = new Vector2(_direction, Mathf.Sin(_verticalDir)) * Speed;
+        transform.Translate(Velocity * Time.deltaTime);
     }
     public void ButtonControl(InputAction.CallbackContext context)
     {
@@ -25,12 +31,10 @@ public class PlayerController : MonoBehaviour
         {
             switch (context.interaction)
             {
-                case TapInteraction:
+                case TapInteraction: // Reverse Direction
                     _direction *= -1;
-                    Debug.Log("Tapped");
                     break;
-                case HoldInteraction:
-                    Debug.Log("Held");
+                case HoldInteraction: // Pause
                     break;
             }
         }
