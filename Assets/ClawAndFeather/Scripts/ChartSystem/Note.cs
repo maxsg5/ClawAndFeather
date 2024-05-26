4bed00e1a64694b0303cad2583e28c4ec69c8ec7
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2024-05-26 15:10:11 -0600

    created note struct and SongChart class

---
+++ Assets/ClawAndFeather/Scripts/ChartSystem/Note.cs
@@ -0,0 +1,13 @@
+public struct Note
+{
+    public float BeatDelay { get; private set; }
+    public float BeatTime { get; private set; }
+    public bool IsRest { get; private set; }
+
+    public Note(float beatDelay, float beatTime, bool isRest)
+    {
+        BeatDelay = beatDelay;
+        BeatTime = beatTime;
+        IsRest = isRest;
+    }
+}
---
+++ Assets/ClawAndFeather/Scripts/ChartSystem/SongChart.cs
@@ -0,0 +1,23 @@
+﻿public class SongChart
+{
+    public float BPM { get; private set; }
+    public float ToleranceEarly { get; private set; }
+    public float ToleranceLate { get; private set; }
+
+    public Note[] Notes { get; private set; }
+
+    public SongChart(float bpm, float toleranceEarly, float toleranceLate, Note[] notes)
+    {
+        BPM = bpm;
+        ToleranceEarly = toleranceEarly;
+        ToleranceLate = toleranceLate;
+        Notes = notes;
+    }
+
+    public bool CheckTolerance(float inputTime, out float accuracy)
+    {
+        accuracy = 0f;
+
+
+    }
+}
