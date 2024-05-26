6a35a9d8ae83f12e224c60fceddb570749fd0b5c
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-05-26 16:21:14 -0600

    New Static Method

--- Assets/ClawAndFeather/Scripts/ChartSystem/SongParser.cs
+++ Assets/ClawAndFeather/Scripts/ChartSystem/SongParser.cs
@@ -5,23 +5,21 @@ using UnityEngine;
 public class SongChartParser : MonoBehaviour
 {
     public TextAsset[] filesToParse;
-    private string[] _dataLines;
     void Start()
     {
-        for (int i = 0; i < filesToParse.Length; i++)
-        {
-            ReadCSVFile(filesToParse[i]);
-        }
+        
     }
 
-    private void ReadCSVFile(TextAsset file)
+    public static bool ReadCSVFile(TextAsset file, out SongChart chart)
     {
-        _dataLines = file.text.Split('\n'); // split by each new line
-        string[] nonRepeatingData = _dataLines[0].Split(',');
+        chart = null;
+        string[] dataLines = file.text.Split('\n'); // split by each new line
+        string[] nonRepeatingData = dataLines[0].Split(',');
         if (nonRepeatingData.Length != 3)
-        { 
+        {
             Debug.LogError("Length of BPM,ToleranceEarly,ToleranceLate does not meet format.");
-            return;
+            
+            return false;
         }
 
         #region Checking the Non Repeating Data
@@ -33,18 +31,18 @@ public class SongChartParser : MonoBehaviour
           || !float.TryParse(nonRepeatingData[2], out lateTolerance))
         {
             Debug.LogError("Error with BPM, ToleranceEarly, or ToleranceLate.");
-            return;
+            return false;
         }
         #endregion
         
         List<Note> notes = new List<Note>();
         float totalBeatTime = 0;
 
-        for (int i = 1; i < _dataLines.Length; i++)
+        for (int i = 1; i < dataLines.Length; i++)
         {
             try
             {
-                string[] row = _dataLines[i].Split(',');
+                string[] row = dataLines[i].Split(',');
                 
                 if (row.Length != 2)
                 { throw new Exception($"Row {i} of file {file.name} is in the incorrect format."); }
@@ -59,16 +57,17 @@ public class SongChartParser : MonoBehaviour
                 else
                 { beatDelay = beatDelay = 60 * timeSignature / (numberOfBeats * bpm); }
 
-                notes.Add(new Note(beatDelay, totalBeatTime, (numberOfBeats == 0) ? true : false ));
+                notes.Add(new Note(beatDelay, totalBeatTime, numberOfBeats == 0 ? true : false ));
                 totalBeatTime += beatDelay;
             }
             catch (Exception e)
             {
                 Debug.LogError(e.Message);
-                return;
+                return false;
             }
         }
 
-        SongChart chart = new SongChart(bpm, earlyTolerance, lateTolerance, notes.ToArray());
+        chart = new SongChart(bpm, earlyTolerance, lateTolerance, notes.ToArray());
+        return true;
     }
 }
