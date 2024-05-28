d9d32f5e3b847836f3fef1267b959901a535ff2c
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-05-28 17:11:35 -0600

    BUTTONS FUCKING DONE

--- Assets/ClawAndFeather/Scripts/MenuScripts/MenuController.cs
+++ Assets/ClawAndFeather/Scripts/MenuScripts/MenuController.cs
@@ -11,16 +11,25 @@ public class MenuController : MonoBehaviour
     [SerializeField] private EventSystem _eventSystem;
     [SerializeField] private GameObject[] _buttonObjects;
     private Button[] _buttons;
+    private Image[] _buttonImages;
     private int _selectedButton;
 
+    private Coroutine _fadeCoroutine;
+    private Coroutine _flashCoroutine;
+
+    public Color submittedButtonColor;
+    public Color selectedButtonColor;
     void Awake()
     {
         _buttons = new Button[_buttonObjects.Length];
+        _buttonImages = new Image[_buttonObjects.Length];
         for (int i = 0; i < _buttonObjects.Length; i++)
         {
             _buttons[i] = _buttonObjects[i].GetComponent<Button>();
+            _buttonImages[i] = _buttonObjects[i].GetComponent<Image>();
         }
         _selectedButton = 0;
+        _fadeCoroutine = StartCoroutine(Fade(_buttonImages[_selectedButton], 1, 0.2f));
         //_buttons[0].onClick.AddListener(() => ChangeScene(1)); //play button
     }
     private void ChangeScene(int ID) => SceneManager.LoadScene(ID);
@@ -31,14 +40,38 @@ public class MenuController : MonoBehaviour
             switch (context.interaction)
             {
                 case TapInteraction: // go to next
+
+                    if (_fadeCoroutine != null)
+                    { StopCoroutine(_fadeCoroutine); }
+
+                    if (_flashCoroutine != null)
+                    { StopCoroutine(_flashCoroutine); }
+
+                    _buttonImages[_selectedButton].color = Color.white;
+
                     _selectedButton = (_selectedButton + 1) % _buttonObjects.Length;
-                    _eventSystem.SetSelectedGameObject(_buttonObjects[_selectedButton]);
+
+                    _fadeCoroutine = StartCoroutine(Fade(_buttonImages[_selectedButton], 1, 0.2f));
+                    //_eventSystem.SetSelectedGameObject(_buttonObjects[_selectedButton]);
+
                     break;
                 case HoldInteraction: // click button
+
                     _buttons[_selectedButton].onClick.Invoke();
-                    StartCoroutine(Flash(_buttonObjects[_selectedButton].GetComponent<Image>(), 1));
+                    _flashCoroutine = StartCoroutine(Flash(_buttonImages[_selectedButton], 1));
+
                     break;
             }
+        }
+    }
+    private IEnumerator Fade(Image image, float fadeDuration, float fadeLerp)
+    {
+        float timePassed = 0;
+        while (timePassed < fadeDuration)
+        {
+            timePassed += Time.deltaTime;
+            image.color = Color.Lerp(image.color, selectedButtonColor, fadeLerp);
+            yield return null;
         }
     }
     private IEnumerator Flash(Image image, int duration)
@@ -47,14 +80,15 @@ public class MenuController : MonoBehaviour
         while (timePassed < duration * 0.5f)
         {
             timePassed += Time.deltaTime;
-            image.color = Color.Lerp(image.color, Color.red, 0.1f);
+            image.color = Color.Lerp(image.color, submittedButtonColor, 0.1f);
             yield return null;
         }
         while (timePassed < duration)
         {
             timePassed += Time.deltaTime;
-            image.color = Color.Lerp(image.color, Color.white, 0.1f);
+            image.color = Color.Lerp(image.color, selectedButtonColor, 0.1f);
             yield return null;
         }
+        image.color = selectedButtonColor;
     }
 }
