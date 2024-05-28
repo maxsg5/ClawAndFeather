f3df8a5912371dd965a755153c1a88e0f7b22650
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2024-05-28 13:57:55 -0600

    Update NoteVisualizer.cs

--- Assets/ClawAndFeather/Scripts/ChartSystem/NoteVisualizer.cs
+++ Assets/ClawAndFeather/Scripts/ChartSystem/NoteVisualizer.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class NoteVisualizer : MonoBehaviour
 {
+    public Color NoNote = Color.white;
+    public Color Early = Color.yellow;
+    public Color Note = Color.green;
+    public Color Late = Color.red;
+
     private Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+}
+
+public class BeatVisualizer : MonoBehaviour
+{
+    public Color onColour = Color.white;
+    public Color offColour = Color.black;
+
+    private void Update()
     {
         
     }
