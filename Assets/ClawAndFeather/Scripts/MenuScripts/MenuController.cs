4e83b787b33cc620d23dba934a7fa28f860f3231
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-05-30 20:05:01 -0600

    Hover added

--- Assets/ClawAndFeather/Scripts/MenuScripts/MenuController.cs
+++ Assets/ClawAndFeather/Scripts/MenuScripts/MenuController.cs
@@ -9,27 +9,42 @@ public class MenuController : MonoBehaviour
     [SerializeField] private GameObject[] _buttonObjects;
     private Button[] _buttons;
     private Image[] _buttonImages;
-    private int _selectedButton;
+    private Animator[] _buttonAnimations;
+    public int selectedButton;
 
     private Coroutine _fadeCoroutine;
     private Coroutine _flashCoroutine;
-
+    [Space]
     public Color submittedButtonColor;
     public Color selectedButtonColor;
+    public Color unselectedButtonColor;
+    
     void Awake()
     {
         _buttons = new Button[_buttonObjects.Length];
         _buttonImages = new Image[_buttonObjects.Length];
+        _buttonAnimations = new Animator[_buttonObjects.Length];
+
         for (int i = 0; i < _buttonObjects.Length; i++)
         {
             _buttons[i] = _buttonObjects[i].GetComponent<Button>();
             _buttonImages[i] = _buttonObjects[i].GetComponent<Image>();
+            _buttonAnimations[i] = _buttonObjects[i].GetComponent<Animator>();
         }
-        _selectedButton = 0;
-        _fadeCoroutine = StartCoroutine(Fade(_buttonImages[_selectedButton], 1, 0.2f));
+        selectedButton = 0;
         _buttons[0].onClick.AddListener(() => GameState.ChangeScene(1)); //play button
         _buttons[_buttonObjects.Length - 1].onClick.AddListener(GameState.ExitGame); // exit game
     }
+    private void OnEnable()
+    {
+        StartCoroutine(Initialize(1));
+    }
+    private IEnumerator Initialize(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        _buttonImages[0].color = selectedButtonColor;
+        _buttonAnimations[0].SetBool("Selected", true);
+    }    
     public void MenuControls(InputAction.CallbackContext context)
     {
         if (context.performed)
@@ -44,16 +59,17 @@ public class MenuController : MonoBehaviour
                     if (_flashCoroutine != null)
                     { StopCoroutine(_flashCoroutine); }
 
-                    _buttonImages[_selectedButton].color = Color.white;
-
-                    _selectedButton = (_selectedButton + 1) % _buttonObjects.Length;
-                    _fadeCoroutine = StartCoroutine(Fade(_buttonImages[_selectedButton], 1, 0.2f));
+                    _buttonImages[selectedButton].color = unselectedButtonColor;
+                    _buttonAnimations[selectedButton].SetBool("Selected", false);
+                    selectedButton = (selectedButton + 1) % _buttonObjects.Length;
+                    _buttonAnimations[selectedButton].SetBool("Selected", true);
+                    _fadeCoroutine = StartCoroutine(Fade(_buttonImages[selectedButton], 1, 0.2f));
 
                     break;
                 case HoldInteraction: // click button
 
-                    _buttons[_selectedButton].onClick.Invoke();
-                    _flashCoroutine = StartCoroutine(Flash(_buttonImages[_selectedButton], 1));
+                    _buttons[selectedButton].onClick.Invoke();
+                    _flashCoroutine = StartCoroutine(Flash(_buttonImages[selectedButton], 1));
 
                     break;
             }
