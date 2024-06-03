74eb8324416e74f930933858f70cc0e47af8a61e
Kael-The-Vegetable <kkostiuk3@nait.ca>
2024-06-02 18:28:28 -0600

    Update TimeScaler.cs

--- Assets/ClawAndFeather/Scripts/GlobalScripts/TimeScaler.cs
+++ Assets/ClawAndFeather/Scripts/GlobalScripts/TimeScaler.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class TimeScaler : MonoBehaviour
@@ -12,4 +14,27 @@ public class TimeScaler : MonoBehaviour
         GameTime = Time.deltaTime * _gameTimeScale;
         UITime = Time.deltaTime * _UITimeScale;
     }
+
+
+    /// <summary>
+    /// Use this method to set a variable followed by waiting, then setting the variable later to a different value. <br />
+    /// Use ( result => [ your variable name here ] = result ) in place of the <paramref name="variable"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="variable"></param>
+    /// <param name="time"></param>
+    /// <param name="initialValue"></param>
+    /// <param name="finalValue"></param>
+    public IEnumerator DelayedGameVarChange<T>(Action<T> variable, float time, T initialValue, T finalValue)
+    {
+        variable(initialValue);
+        float currentTime = 0;
+        while (currentTime < time)
+        {
+            currentTime += GameTime;
+            yield return null;
+        }
+        variable(finalValue);
+    }
+
 }
