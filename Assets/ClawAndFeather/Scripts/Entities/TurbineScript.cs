9b4a40aa2eb83caa8faf6d708fb830cf9a64b5b5
RandyButternubszz <144160681+RandyButternubszz@users.noreply.github.com>
2024-06-03 14:46:16 -0600

    UFO prefab completed (Collider/Splines)

--- Assets/ClawAndFeather/Scripts/Entities/TurbineScript.cs
+++ Assets/ClawAndFeather/Scripts/Entities/TurbineScript.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[AddComponentMenu("Scripts/Entities/Turbine")]
 public class TurbineScript : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
