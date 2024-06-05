57bc8766c2ca65f3a7d97809519c5b72d42636f7
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2024-06-05 13:01:47 -0600

    Updated alien to add gizmos

--- Assets/ClawAndFeather/Scripts/Entities/Alien.cs
+++ Assets/ClawAndFeather/Scripts/Entities/Alien.cs
@@ -1,13 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[AddComponentMenu("Scripts/Claw and Feather/Entities/Turbine")]
+[AddComponentMenu("Scripts/Claw and Feather/Entities/Alien Cat")]
 [RequireComponent(typeof(Rigidbody2D))]
 public class Alien : MonoBehaviour
 {
     #region Inspector
     public Transform target;
+    [SerializeField] private bool _autoDetectPlayer = true;
 
     [Header("Movement Settings")]
     [Min(0)] public float moveSpeed = 5.0f;
@@ -15,19 +14,26 @@ public class Alien : MonoBehaviour
     [Range(0, 360)] public float turnSpeed = 30.0f;
 
     [Header("Detection Settings")]
-    [Min(0)] public float detectionRange;
+    [Min(0)] public float detectionRange = 3.0f;
     [Min(0)] public float pursuitTime = 1.0f;
-    [SerializeField] private float _pursuitTimer = 0.0f;
+
+    [Header("Gizmo Settings")]
+    [SerializeField] private Color _colour = Color.white;
+    [SerializeField] private bool _drawAlways = false;
+    [Space]
+    [SerializeField] private bool _showDetectionRange = true;
+    [SerializeField] private bool _showTargetLine = false;
     #endregion
 
     private Rigidbody2D _rb;
+    private float _pursuitTimer = 0.0f;
 
     public bool TargetInRange => Vector3.Distance(transform.position, target.position) < detectionRange;
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        if (target == null)
+        if (_autoDetectPlayer)
         {
             target = GameObject.FindGameObjectWithTag("Player").transform;
         }
@@ -55,6 +61,36 @@ public class Alien : MonoBehaviour
         else
         {
             _pursuitTimer = 0;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (!_drawAlways)
+        {
+            DrawGizmos();
+        }
+    }
+
+    private void DrawGizmos()
+    {
+        Gizmos.color = _colour;
+        if (_showDetectionRange)
+        {
+            Gizmos.DrawWireSphere(transform.position, detectionRange);
+        }
+
+        if (_showTargetLine)
+        {
+            Gizmos.DrawLine(transform.position, target.position);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_drawAlways)
+        {
+            DrawGizmos();
         }
     }
 }
